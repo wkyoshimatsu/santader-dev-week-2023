6baec9ec3cd6c085eb04a1f8fddae89ea1276dbf
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-08-14 12:47:31 -0300

    Updated - Application.java location - UserRepository.java annotation

--- src/main/java/me/wky/Application.java
+++ src/main/java/me/wky/Application.java
@@ -1,4 +1,4 @@
-package me.wky.domain;
+package me.wky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/me/wky/domain/repository/UserRepository.java
+++ src/main/java/me/wky/domain/repository/UserRepository.java
@@ -2,7 +2,9 @@ package me.wky.domain.repository;
 
 import me.wky.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByAccountNumber(String accountNumber);

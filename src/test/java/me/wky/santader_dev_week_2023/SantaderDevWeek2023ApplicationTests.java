21a832ee7df84fe42b0d520468ced488ec94ade0
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-08-14 09:35:34 -0300

    Initial commit

---
+++ src/test/java/me/wky/santader_dev_week_2023/SantaderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wky.santader_dev_week_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
